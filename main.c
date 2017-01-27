f52bb7e6dffa9996beaeec985dbf0c4056b99702
prost_m <matthias.prost@epitech.eu>
2017-01-27 17:14:27 +0100

    Script de push

--- main.c
+++ main.c
@@ -5,48 +5,46 @@
 ** Login   <matthias.prost@epitech.eu>
 **
 ** Started on  Wed Jan 25 14:58:41 2017 Matthias Prost
-** Last update Thu Jan 26 15:20:15 2017 Matthias Prost
+** Last update Fri Jan 27 16:52:46 2017 Matthias Prost
 */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 
-void		*my_malloc(size_t size)
+void *malloc(size_t sz)
 {
-  if (brk(sbrk(size)) == -1)
-    {
-      printf("Done!");
-      return (NULL);
-    }
-  else
-    return (sbrk(size));
-  return (NULL);
+    void *mem;
+    static unsigned char our_memory[1024 * 1024];
+    static size_t next_index = 0;
+
+    if(sizeof our_memory - next_index < sz)
+        return NULL;
+
+    mem = &our_memory[next_index];
+    next_index += sz;
+    return mem;
 }
 
-int		main()
+void	free(void *ptr)
 {
-  char	*str;
-  char	*temp;
-  int		i;
-  char	*buff;
-
-  if ((str = my_malloc(sizeof(char) * 5)) == NULL)
-    {
-      printf("ERROR MALLOC\n");
-      return 0;
-    }
-  if ((temp = my_malloc(sizeof(char) * 5)) == NULL)
-    {
-      printf("ERROR MALLOC\n");
-      return 0;
-    }
-  buff = "Hello";
-  i = -1;
-  while (buff[++i])
-    {
-      str[i] = buff[i];
-      str[i] = temp[i];
-    }
-  printf("%s\n%s\n", buff, temp);
+  ptr = ptr;
 }
+// 
+// int		main()
+// {
+//   char	*str;
+//   int		i;
+//   char	*buff;
+//
+//   if ((str = my_malloc(sizeof(char) * 5)) == NULL)
+//     {
+//       printf("ERROR MALLOC\n");
+//       return 0;
+//     }
+//   buff = "Hello";
+//   i = -1;
+//   while (buff[++i])
+//     str[i] = buff[i];
+//   printf("%s\n", buff);
+// }
