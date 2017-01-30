66969ba35cb356e7c38f187562cb60b5ffd4a781
prost_m <matthias.prost@epitech.eu>
2017-01-30 17:53:22 +0100

    Script de push

--- src/realloc.c
+++ src/realloc.c
@@ -5,7 +5,7 @@
 ** Login   <matthias.prost@epitech.eu>
 **
 ** Started on  Mon Jan 30 17:25:21 2017 Matthias Prost
-** Last update Mon Jan 30 17:35:13 2017 Matthias Prost
+** Last update Mon Jan 30 17:50:56 2017 Matthias Prost
 */
 
 #include "malloc.h"
@@ -17,5 +17,14 @@ void	*realloc(void *ptr, size_t size)
 
   if (!ptr)
     return (malloc(size));
-  if ()
+  list = (t_list *)ptr - 1;
+  if (list->size >= size)
+    return (ptr);
+  buff = malloc(size);
+  if (buff)
+    {
+      memcpy(buff, ptr, header->size);
+      free(ptr);
+    }
+  return buff;
 }
